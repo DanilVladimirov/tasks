8df04f7789d6d9d3153004bb59f3882f4725b741
Danil Vladimirov <danilvladimirovnkk@gmail.com>
2021-06-30 07:16:14 +0300

    edit task1

--- task1/task1.py
+++ task1/task1.py
@@ -1,16 +1,14 @@
 """напишите функцию slice_less,
 на вход принимает:
-	1. первый аргумент “my_list” - список из целых чисел
-	2. второй аргумент “lesser” - целое число
+1. первый аргумент “my_list” - список из целых чисел
+2. второй аргумент “lesser” - целое число
 выводит:
 отсортированный от большего к меньшему список my_list, где все значения больше чем lesser
 """
 
 
 def slice_less(my_list, lesser):
-    new_list = [x for x in my_list if x > lesser]
-    new_list.sort(reverse=True)
-    print(new_list)
+    print(sorted([x for x in my_list if x > lesser], reverse=True))
 
 
 if __name__ == '__main__':
